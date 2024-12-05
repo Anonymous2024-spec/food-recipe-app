82c1ca7db4dee38f3baef58b15e08081ba741cd9
Anonymous2024-spec <shifahnana2@gmail.com>
2024-12-05 11:19:40 +0300

    second commit

--- src/components/FoodDetails.jsx
+++ src/components/FoodDetails.jsx
@@ -5,6 +5,7 @@ export default function FoodDetails({ foodId }) {
   const API_KEY = "37aa341159af4b2d88e658cdc3079bb5";
 
   const [food, setFood] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     async function fetchFood() {
@@ -12,15 +13,39 @@ export default function FoodDetails({ foodId }) {
       const data = await res.json();
       console.log(data);
       setFood(data);
+      setIsLoading(false);
     }
     fetchFood();
   }, [foodId]);
 
   return (
     <div>
-      Food Details {foodId}
-      {food.title}
-      <img src={food.image} alt="" />
+      <div>
+        <h1>{food.title}</h1>
+
+        <img src={food.image} alt="" />
+        <div>
+          <span>
+            <strong>🕒 {food.readyInMinutes} Minutes</strong>
+          </span>
+          <span>
+            👨‍👩‍👦‍👦<strong>Serves {food.servings}</strong>
+          </span>
+          <span>{food.vegetarian ? "🥕 Vegetarian" : "🍗 Not Vegetarian"}</span>
+          <span>{food.vegan ? "🐮 Vegan" : ""}</span>
+        </div>
+        <div>
+          $<span>{food.pricePerServing / 100}</span>
+        </div>
+      </div>
+      <div>
+        <h2>Instructions</h2>
+        {isLoading ? (
+          <p>Loading.....</p>
+        ) : (
+          food.analyzedInstructions[0].steps.map((item) => <li>{item.step}</li>)
+        )}
+      </div>
     </div>
   );
 }
